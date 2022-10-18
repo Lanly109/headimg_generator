e6bf2f10fc3ee32f944b063085d4cf7eac5c302c
kcn3388 <kcn3388@126.com>
2022-10-18 09:37:27 +0800

    fix conflict

--- __init__.py
+++ __init__.py
@@ -3,8 +3,9 @@ import re
 import shlex
 import traceback
 from typing import List
+from io import BytesIO
 
-from hoshino import Service, HoshinoBot
+from hoshino import Service, HoshinoBot, priv
 from hoshino.typing import CQEvent, MessageSegment
 from nonebot import on_startup
 
@@ -12,17 +13,38 @@ from .data_source import make_image, commands
 from .models import UserInfo
 from .utils import help_image
 
-sv = Service('头像表情包', help_='''
-![](https://s2.loli.net/2022/09/18/zrhvqdCMDyOSpZj.jpg)
-'''.strip(), enable_on_default=True, bundle='娱乐', visible = True)
+sv_help = """
+[头像表情包] 发送全部功能帮助
+"""
 
-@sv.on_fullmatch(("帮助头像表情包"))
-async def help(bot, ev):
-    img = await help_image(commands)
-    base64_str = base64.b64encode(img.getvalue()).decode()
-    img =  'base64://' + base64_str
-    img = str(MessageSegment.image(img))
-    await bot.send(ev, img)
+cmd_prefix = "##"
+
+sv = Service(
+    name="头像表情包",
+    use_priv=priv.NORMAL,  # 使用权限
+    manage_priv=priv.ADMIN,  # 管理权限
+    visible=True,  # False隐藏
+    enable_on_default=True,  # 是否默认启用
+    bundle='娱乐',  # 属于哪一类
+    help_=sv_help  # 帮助文本
+)
+
+
+@sv.on_fullmatch(["帮助头像表情包"])
+async def bangzhu(bot, ev):
+    await bot.send(ev, sv_help, at_sender=True)
+
+
+def bytesio2b64(im: BytesIO) -> str:
+    im = im.getvalue()
+    return f"base64://{base64.b64encode(im).decode()}"
+
+
+@sv.on_fullmatch("头像表情包")
+async def bangzhu(bot: HoshinoBot, ev: CQEvent):
+    im = await help_image(commands)
+    await bot.send(ev, MessageSegment.image(bytesio2b64(im)))
+
 
 def is_qq(msg: str):
     return msg.isdigit() and 11 >= len(msg) >= 5
@@ -53,24 +75,23 @@ class Handler:
         args: List[str] = []
         msg = event.message
 
-        # 由于使用的trigger是prefix，回复时无法捕捉trigger，不兼容
         # 回复前置处理
-        # if msg[0].type == "reply":
-        #     # 当回复目标是自己时，去除隐式at自己
-        #     if msg[0].data["qq"] == str(event.user_id):
-        #         msg.pop(1)
-        #     # 因为回复别人会默认多加一个at，需要跳过回复附带的显式at
-        #     elif len(msg) > 3:
-        #         temp_msg = [msg[0]] + [each for each in msg[3:]]
-        #         at_sb = MessageSegment.at(msg[0].data["qq"])
-        #         if temp_msg[1] == at_sb:
-        #             temp_msg.pop(1)
-        #         msg = temp_msg
-        #     # 手机版可以去掉显式at，因此直接去除隐式at即可
-        #     else:
-        #         at_sb = MessageSegment.at(msg[0].data["qq"])
-        #         if msg[1] == at_sb:
-        #             msg.pop(1)
+        if msg[0].type == "reply":
+            # 当回复目标是自己时，去除隐式at自己
+            if msg[0].data["qq"] == str(event.user_id):
+                msg.pop(1)
+            # 因为回复别人会默认多加一个at，需要跳过回复附带的显式at
+            elif len(msg) > 3:
+                temp_msg = [msg[0]] + [each for each in msg[3:]]
+                at_sb = MessageSegment.at(msg[0].data["qq"])
+                if temp_msg[1] == at_sb:
+                    temp_msg.pop(1)
+                msg = temp_msg
+            # 手机版可以去掉显式at，因此直接去除隐式at即可
+            else:
+                at_sb = MessageSegment.at(msg[0].data["qq"])
+                if msg[1] == at_sb:
+                    msg.pop(1)
 
         for msg_seg in msg:
             if msg_seg.type == "at":
@@ -98,7 +119,8 @@ class Handler:
                 raw_text = str(msg_seg)
                 try:
                     texts = shlex.split(raw_text)
-                except:
+                except Exception as e:
+                    sv.logger.warning(f"{e}")
                     texts = raw_text.split()
                 for text in texts:
                     if is_qq(text):
@@ -115,12 +137,13 @@ class Handler:
                         if text:
                             args.append(text)
 
+        args.pop(0)
         if len(args) > self.command.arg_num:
             return False
         if event.raw_message.find(f"[CQ:at,qq={str(event.self_id)}") != -1:
             users.append(UserInfo(qq=str(event.self_id), group=str(event.group_id)))
         if not users:
-            return False
+            users.append(UserInfo(qq=str(event.self_id), group=str(event.group_id)))
 
         sender = UserInfo(qq=str(event.user_id))
         await get_user_info(bot, sender)
@@ -129,9 +152,9 @@ class Handler:
             await get_user_info(bot, user)
 
         try:
-            img = await make_image(command = self.command, sender = sender, users = users, args = args)
+            img = await make_image(command=self.command, sender=sender, users=users, args=args)
             base64_str = base64.b64encode(img.getvalue()).decode()
-            img =  'base64://' + base64_str
+            img = 'base64://' + base64_str
             img = str(MessageSegment.image(img))
         except Exception as e:
             print(traceback.format_exc())
@@ -139,12 +162,16 @@ class Handler:
 
         await bot.send(event, img)
 
+
 @on_startup
 async def register_handler():
     for command in commands:
-        func = getattr(sv, "on_prefix")
+        func = getattr(sv, "on_keyword")
         key = command.keywords
-        func = func(key, only_to_me=False)
+        keys = []
+        if cmd_prefix:
+            for each in key:
+                keys.append(f"{cmd_prefix}{each}")
+        func = func(keys, only_to_me=False)
         func(Handler(command).handle)
-    print('petpet register done.')
-
+    sv.logger.info('petpet register done.')
