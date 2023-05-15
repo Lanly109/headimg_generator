2e4c001577ac3c7256fdf733743eb60143c28ae9
kcn3388 <kcn3388@126.com>
2023-05-15 17:18:44 +0800

    prevent import submodule error

--- meme_optional/__init__.py
+++ meme_optional/__init__.py
@@ -3,4 +3,7 @@ from .cuidao import *
 from .operations import *
 
 if os.path.exists(os.path.join(os.path.dirname(__file__), "meme_3rd_optional")):
-    from .meme_3rd_optional import *
+    try:
+        from .meme_3rd_optional import *
+    except ModuleNotFoundError:
+        pass
