485fe8c6140184176d8dd3f273518001a65a2c31
霞飛 <saarchaffee@qq.com>
2025-06-10 00:30:36 +0800

    fix: set default as document

--- config.py
+++ config.py
@@ -4,13 +4,15 @@ from meme_generator.config import meme_config
 
 meme_command_start: str = ""  # 命令前缀
 memes_prompt_params_error: bool = (
-    True  # 是否在图片/文字数量不符或参数解析错误时提示（若没有设置命令前缀不建议开启，否则极易误触发）
+    False  # 是否在图片/文字数量不符或参数解析错误时提示（若没有设置命令前缀不建议开启，否则极易误触发）
 )
-memes_check_resources_on_startup: bool = True  # 是否在启动时检查 `meme-generator` 资源
+memes_check_resources_on_startup: bool = False  # 是否在启动时检查 `meme-generator` 资源
 memes_use_sender_when_no_image: bool = (
-    True  # 在表情需要至少1张图且没有输入图片时，是否使用发送者的头像（谨慎使用，容易误触发）
+    False  # 在表情需要至少1张图且没有输入图片时，是否使用发送者的头像（谨慎使用，容易误触发）
 )
 memes_use_default_when_no_text: bool = (
+    False  # 在表情需要至少1段文字且没有输入文字时，是否使用默认文字（谨慎使用，容易误触发）
+)
 memes_normal_error: bool = (
     False  # 是否输出表情的相关错误提示，如获取图片失败、表情不存在等（谨慎使用，容易误触发）
 )
